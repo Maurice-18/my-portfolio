33ad00b0b5a66b707466fc473ce314661a985539
Maurice Mwangi <mwangimuita18@gmail.com>
2025-12-04 11:27:10 +0300

    second updates

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
       <Footer />
     </div>
   );
